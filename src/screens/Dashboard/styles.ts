d63ac5fc4c0fc6840039543a5ac6e11f4444ed3b
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-29 22:40:38 -0300

    feat: create a a new directory 'Global'

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+    flex: 1;
+    background-color: #fff;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.Text`
+    font-size: 24px;
+    font-weight: bold;
+    color: #000;
+`;
