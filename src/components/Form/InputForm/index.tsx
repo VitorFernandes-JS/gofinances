5536330c0231c90c19459766b5b8995ae66401ae
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-10-06 13:39:20 -0300

    fix: finish component InputForm

---
+++ src/components/Form/InputForm/index.tsx
@@ -0,0 +1,26 @@
+// importações nativas
+import React from "react";
+import { Container } from "./styles";
+import { TextInputProps } from "react-native";
+// componentes
+import { Input } from "../Input";
+// libs
+import { Control, Controller } from "react-hook-form";
+interface Props extends TextInputProps {
+  control: Control;
+  name: string;
+}
+
+export function InputForm({ control, name, ...rest }: Props) {
+  return (
+    <Container>
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <Input onChangeText={onChange} value={value} {...rest} />
+        )}
+        name={name}
+      />
+    </Container>
+  );
+}
