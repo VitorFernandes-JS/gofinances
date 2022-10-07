4f2ecd2704112597de06fc3844fe6155a1dcca34
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-10-07 13:03:28 -0300

    fix: including async storage for inputs

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,5 @@
 // importações nativas
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Alert, Modal, TouchableWithoutFeedback, Keyboard } from "react-native";
 import {
   Container,
@@ -19,8 +19,11 @@ import { CategorySelect } from "../CategorySelect";
 import { useForm } from "react-hook-form";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import uuid from "react-native-uuid";
+import { useNavigation } from "@react-navigation/native";
 
-interface FormData  {
+interface FormData {
   name: string;
   amount: string;
 }
@@ -28,7 +31,9 @@ interface FormData  {
 const schema = Yup.object().shape({
   name: Yup.string().required("Nome é obrigatório"),
   amount: Yup.number()
-    .transform((_value, originalValue) => Number(originalValue.replace(/,/, '.'))) //converte a virgula em ponto
+    .transform((_value, originalValue) =>
+      Number(originalValue.replace(/,/, "."))
+    ) //converte a virgula em ponto
     .typeError("Informe um valor numérico")
     .positive("O valor não pode ser negativo")
     .required("O valor é obrigatório"),
@@ -39,44 +44,71 @@ export function Register() {
     key: "category",
     name: "Categoria",
   });
+  const navigation = useNavigation();
   const [transactionType, setTransactionType] = useState("");
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
-
-  function handleRegister(form: any) {
-    if (!transactionType)
-      return Alert.alert(
-        "Ops, você esqueceu de selecionar o tipo da transação!"
-      );
-    if (category.key === "category")
-      return Alert.alert("Ops, você esqueceu de selecionar a categoria!");
-    const data = {
-      name: form.name,
-      amount: form.amount,
-      transactionType,
-      category: category.key,
-    }
-    console.log(data);
-  };
-
+  const dataKey = "@gofinances:transactions";
+  
   function handleTransactionTypeSelect(type: "up" | "down") {
     setTransactionType(type);
-  }
+  };
 
   function handleOpenSelectCategoryModal() {
     setCategoryModalOpen(true);
-  }
+  };
 
   function handleCloseSelectCategoryModal() {
     setCategoryModalOpen(false);
-  }
+  };
 
-  const { 
-    control, 
+  const {
+    control,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
+
+  async function handleRegister(form: FormData) {
+
+    if (!transactionType)
+      return Alert.alert("Ops, você esqueceu de selecionar o tipo da transação!");
+
+    if (category.key === "category")
+      return Alert.alert("Ops, você esqueceu de selecionar a categoria!");
+
+    const newTransaction = {
+      id: String(uuid.v4()),
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key,
+      date: new Date(),
+    };
+
+    try {
+      const data = await AsyncStorage.getItem(dataKey); //pega os dados do storage
+      const currentData = data ? JSON.parse(data) : []; // se tiver dados, converte para objeto, se não, retorna um array vazio
+      
+      const dataFormatted = [...currentData, newTransaction]; // concatena o novo objeto com o array de objetos
+      
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted)); // salva os dados no storage
+
+      reset(); // limpa os campos do formulário
+      setTransactionType(""); // limpa os campos do formulário
+      setCategory({
+        key: "category",
+        name: "Categoria",
+      }); // limpa os campos do formulário
+
+      navigation.navigate("Listagem"); // navega para a tela de listagem
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível cadastrar");
+    }
+  };
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
