67d5adef59149a1fe6be660ebcf868340be9dcac
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-10-27 12:26:55 -0300

    feat: new component HistoryCard

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,6 +7,7 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
+import { Resume } from "../screens/Resume";
 
 export function AppRoutes() {
   const theme = useTheme();
@@ -50,7 +51,7 @@ export function AppRoutes() {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => (
             <MaterialIcons
---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Header, Title } from "./styles";
+
+export function Resume() {
+  return (
+    <Container>
+      <Header>
+        <Title>Resumo</Title>
+      </Header>
+    </Container>
+  );
+}
