79c70ebfe732b14b8162555f13b4de7ac99a66d2
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-11-14 00:42:57 -0300

    fix: changes in Resume

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { ActivityIndicator } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { HistoryCard } from "../../components/HistoryCard";
